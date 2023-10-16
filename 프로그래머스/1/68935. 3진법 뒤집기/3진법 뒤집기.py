ca9fb6049665e74e065ba566d2a39bf3923fc427
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2023-10-17 01:21:23 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -9,3 +9,18 @@ def solution(n):
     for i in range(len(list_s)):
         answer = answer + (3**(len(list_s)-i-1))*list_s[i]
     return answer
+
+
+# def solution(n):
+#     answer = []
+#     while True:
+#         if n < 3:
+#             answer.append(n)
+#             break
+#         answer.append(n % 3)
+#         n = n // 3
+#     answer.reverse()
+#     sum = 0
+#     for i in range(len(answer)):
+#         sum += (answer[i] * (3 ** i))
+#     return sum
