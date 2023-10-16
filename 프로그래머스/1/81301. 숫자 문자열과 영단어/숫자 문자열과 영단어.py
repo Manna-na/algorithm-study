2bbc406e9cd87428a83ffbdada6b9d4797a07a77
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2023-10-17 00:53:36 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,8 +1,15 @@
 def solution(s):
-    answer = 0
     englishes = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     for english in range(len(englishes)):
         if englishes[english] in s:
             s = s.replace(englishes[english], str(english))
     s = int(s)
     return s
+
+# def solution(s):
+#     words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+#     for i in range(len(words)):
+#         s = s.replace(words[i], str(i))
+
+#     return int(s)
