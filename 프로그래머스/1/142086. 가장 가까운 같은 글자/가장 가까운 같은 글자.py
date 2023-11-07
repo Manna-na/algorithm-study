84cecb11121f9fc6695e35f780077be1702e3d9a
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2023-11-08 00:34:36 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -1,12 +1,11 @@
 def solution(s):
     answer = []
-    last_seen = {}
-    
-    for i, char in enumerate(s):
-        if char in last_seen:
-            answer.append(i - last_seen[char])
+    find_val = {}
+    for i, val in enumerate(s):
+        if val in find_val:
+            answer.append(i-find_val[val])
         else:
             answer.append(-1)
-        last_seen[char] = i
-
+        find_val[val] = i
     return answer
+
