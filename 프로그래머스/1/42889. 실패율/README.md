e68a9ef6be32783053675e7cb198896489e27e35
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-01-06 07:05:19 +0900

    [level 1] Title: 실패율, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.02 ms
+메모리: 10.2 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 6일 2:56:51
+2024년 1월 6일 7:5:17
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,18 +1,23 @@
+# 1단계 : 각 스테이지에서 클리어하지 못한 사용자 count
+# 2단계 : 실패율 구하기 = 스테이지 도달 but, fail player / 스테이지 도달한 플레이어
+# 3단계 : 실패율 높은 스테이지부터 내림차순 정렬
+# 4단계 : 스테이지 번호 담긴 배열 반환
+
 def solution(N, stages):
-    answer = []
-    dict = {}
     total = len(stages)
+    ratio = {}
     for i in range(1, N+1):
         fail = stages.count(i)
-        if total == 0:
-            dict[i] = 0
+        if total > 0:
+            ratio[i] = fail / total
         else:
-            dict[i] = fail/total
+            ratio[i] = 0
         total -= fail
-
-    sorted_dict = sorted(dict.items(), key=lambda x:x[1], reverse=True)
-    print(sorted_dict)
-    for i in sorted_dict:
+    answer = []
+    sorted_ratio = sorted(ratio.items(), key=lambda x:x[1], reverse=True)
+    for i in sorted_ratio:
         answer.append(i[0])
-    print(answer)
     return answer
+
+print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3])) #[3,4,2,1,5]
+print(solution(4, [4,4,4,4,4])) #[4,1,2,3]
