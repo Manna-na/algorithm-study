cc96b5732951b107b19198c5f3e9e0efd278133d
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-01-10 03:07:00 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,29 +1,3 @@
-    
 def solution(s):
-    answer = 0
-    arr = list(s)
-    for i in range(len(s)):
-        stack = []
-        arr = s[i:len(s)]+s[0:i]
-        for j in arr:
-            if j == "]":
-                if stack and stack[-1] == "[":
-                    stack.pop()
-                else:
-                    stack.append(j)
-            elif j == "}":
-                if stack and stack[-1] == "{":
-                    stack.pop()
-                else:
-                    stack.append(j)
-            elif j == ")":
-                if stack and stack[-1] == "(":
-                    stack.pop()
-                else:
-                    stack.append(j)
-            else:
-                stack.append(j)
-        if len(stack) == 0:
-            answer += 1
-            
+    answer = -1
     return answer
