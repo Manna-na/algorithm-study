248cad98c299ddc16352f6091396c27e316aa52d
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-01-16 00:23:41 +0900

    [level Lv.1] Title: 가장 가까운 같은 글자, Time: 1.94 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,11 @@
+def solution(s):
+    dict = {}
+    answer = []
+    for i in range(len(s)):
+        if s[i] in dict:
+            answer.append(i - dict[s[i]])
+        else:
+            answer.append(-1)
+        dict[s[i]] = i     
+    return answer
+
