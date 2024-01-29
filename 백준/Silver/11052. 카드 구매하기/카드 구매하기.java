da7bc38bf9bc8db79d90ff4622e7c7dfed9e437c
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-01-29 23:56:37 +0900

    [Silver I] Title: 카드 구매하기, Time: 200 ms, Memory: 17024 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int N = sc.nextInt();
+	int[] p = new int[N+1];
+	int[] dp = new int[N+1];
+	for(int i = 1; i<=N; i++) {
+		p[i] = sc.nextInt();
+	}
+	for(int i = 1;i<=N; i++) {
+		for(int j = 1; j<=i; j++) {
+			dp[i] = Math.max(dp[i], p[j]+dp[i-j]);
+		}
+	}
+	System.out.println(dp[N]);
+}
+}
