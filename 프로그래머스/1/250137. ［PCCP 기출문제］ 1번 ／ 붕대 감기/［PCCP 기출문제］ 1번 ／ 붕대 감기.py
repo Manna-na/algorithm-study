6d8862c30a864361155260ebde6d6f590f7bef37
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-02-14 02:20:26 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.20 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -0,0 +1,31 @@
+def solution(bandage, health, attacks):
+    power = health
+    attack_count = len(attacks)
+    attack_idx = 0
+    bandage_count = 0
+    for i in range(attacks[-1][0]+1):
+        if attacks[attack_idx][0] != i: # 공격 아닐 때
+            bandage_count += 1
+            if bandage_count < bandage[0]: # 시전 시간 이전
+                if power + bandage[1] < health:
+                    power += bandage[1]
+                else:
+                    power = health
+                # bandage_count += 1
+            else: # 시전 시간
+                if power + bandage[1] + bandage[2] < health:
+                    power += bandage[1] + bandage[2]
+                else:
+                    power = health
+                bandage_count = 0
+        else: # 공격일 때
+            power -= attacks[attack_idx][1]
+            bandage_count = 0
+            if power <= 0:
+                return -1
+            if attack_idx + 1 == attack_count:
+                return power
+            attack_idx += 1
+
+                
+                
