640e88244902c5458d272ead00a7b88bd3e44a29
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-02-22 21:37:43 +0900

    [level 2] Title: 짝지어 제거하기, Time: 278.64 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,6 +1,9 @@
 def solution(s):
-    answer = -1
-    
+    stack = []
+    for c in s:
+        if len(stack) > 0 and stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
 
-
-    return answer
+    return int(not stack)
