5e0acbeb3680ab44022b5157067772c73b47bebd
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-02-22 22:05:24 +0900

    [level 2] Title: 주식가격, Time: 29.41 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,8 +1,12 @@
 def solution(prices):
-    answer = [0 for _ in range(len(prices))]
-    for i in range(len(prices)):
-        for j in range(i+1,len(prices)):
-            answer[i] += 1
-            if prices[i] > prices[j]:
-                break
+    answer = [0] * len(prices)
+    stack = [0]
+    for p in range(1, len(prices)):
+        while stack and prices[p] < prices[stack[-1]]:
+            s_idx = stack.pop()
+            answer[s_idx] = p-s_idx
+        stack.append(p)
+    while stack:
+        s_idx = stack.pop()
+        answer[s_idx] = len(prices)-s_idx-1
     return answer
