4ce9cfea45d80c5f8077ecdffd397423644e72cf
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-03-18 00:09:25 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,14 +1,21 @@
+# O(N^2)
+# 1. move열에 해당하는 board 값 중 0이 아닌 값을 찾아서 stack에 넣기
+# 1-1. 모든 값이 0이면 pass
+# 2. stack의 top과 board의 값이 같으면 터트리기 +2
+
 def solution(board, moves):
     answer = 0
     stack = []
     for m in moves:
-        for i in range(len(board)):
-            if board[i][m-1] > 0:
-                if stack and stack[-1] == board[i][m-1]:
-                    stack.pop()
+        for j in range(len(board[0])):
+            if board[j][m-1] > 0:
+                if stack and stack[-1] == board[j][m-1]:
                     answer += 2
+                    stack.pop()
+                elif stack and stack[-1] != board[j][m-1]:
+                    stack.append(board[j][m-1])
                 else:
-                    stack.append(board[i][m-1])
-                board[i][m-1] = 0
+                    stack.append(board[j][m-1])
+                board[j][m-1] = 0
                 break
     return answer
