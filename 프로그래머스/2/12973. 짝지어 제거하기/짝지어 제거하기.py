be3e9ee5f31227905af1b7dd9254cfbf40845859
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-03-17 16:02:38 +0900

    [level 2] Title: 짝지어 제거하기, Time: 93.55 ms, Memory: 14.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,9 +1,16 @@
-def solution(s):
-    stack = []
-    for c in s:
-        if len(stack) > 0 and stack[-1] == c:
-            stack.pop()
-        else:
-            stack.append(c)
+# 1. 스택이 비어있지 않고 현재 값과 stack의 [-1] 값이 같으면 제거
+# 2. 스택 비었는지 확인
 
-    return int(not stack)
+def solution(s):
+    # 10^6 -> O(NlogN)
+    s_stack = []
+    for i in s:
+        if s_stack and s_stack[-1] == i:
+            s_stack.pop()
+        else:
+            s_stack.append(i)
+    if s_stack:
+        return 0
+    else:
+        return 1
+    
