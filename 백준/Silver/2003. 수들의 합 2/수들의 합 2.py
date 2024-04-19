e52c942623a9fcb7cafeb67d5d097a7f0b2898c5
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-04-20 00:01:45 +0900

    [Silver IV] Title: 수들의 합 2, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -6,15 +6,23 @@ array = list(map(int, input().split()))
 ans = 0
 # 슬라이딩 윈도우
 start, end = 0,0
+now_sum = array[start]
 while end < n:
-    curr_sum = sum(array[start:end+1])
-    if curr_sum == m:
+    if now_sum == m:
         ans += 1
+        now_sum -= array[start] # 이전꺼 빼
         start += 1
+        if end+1 >= n:
+            break
         end += 1
-    elif curr_sum > m:
+        now_sum += array[end] # 이후꺼 더해
+    elif now_sum > m:
+        now_sum -= array[start]
         start += 1
     else:
+        if end+1 >= n:
+            break
         end += 1
+        now_sum += array[end]
 
 print(ans)
