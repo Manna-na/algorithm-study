bce75e5e8e7353ddc7ff1e1bb398b340ce8e02ae
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-04-22 15:23:21 +0900

    [Silver V] Title: 오늘도 졌다, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14582. 오늘도 졌다/오늘도 졌다.py
+++ 백준/Silver/14582. 오늘도 졌다/오늘도 졌다.py
@@ -4,23 +4,19 @@ input = sys.stdin.readline
 geminis = list(map(int, input().split()))
 startlink = list(map(int, input().split()))
 
-
+isFlag = False
 geminis_sum, startlink_sum = 0, 0
 geminis_win = False
-count = 0
-
 for i in range(9):
     if geminis[i] + startlink[i] == 0:
         continue
     geminis_sum += geminis[i]
-    # print((i+1),"회 초 - geminis :", geminis_sum, "startlink :", startlink_sum)
     if geminis_sum > startlink_sum:
-        # geminis_win = True
-        count += 1
-    startlink_sum += startlink[i]
-    # print((i + 1), "회 말 - geminis :", geminis_sum, "startlink :", startlink_sum)
+        geminis_win = True
 
-if geminis_sum < startlink_sum and count > 0:
+    startlink_sum += startlink[i]
+
+if geminis_sum < startlink_sum and geminis_win:
     print('Yes')
 else:
     print('No')
