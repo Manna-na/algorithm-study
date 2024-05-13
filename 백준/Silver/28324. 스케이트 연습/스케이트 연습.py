43fb2daeff9425a7f2365f008830790404bd8355
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-05-14 00:00:32 +0900

    [Silver IV] Title: 스케이트 연습, Time: 408 ms, Memory: 86564 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+V = []
+temp = list(map(int, input().split()))
+
+for i in range(N-1, -1, -1):
+    V.append(int(temp[i]))
+
+answer = 1
+cur_speed = 1
+
+for i in range(1, N):
+
+    if V[i] > cur_speed:
+        cur_speed += 1
+    else:
+        cur_speed = V[i]
+
+    answer += cur_speed
+
+print(answer)
