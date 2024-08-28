5436fc65d59d21c068755127e6ad799fe3218795
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2024-08-28 23:36:23 +0900

    [Silver III] Title: 두 수의 합, Time: 88 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -2,22 +2,17 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-array = list(map(int, input().split()))
+ls = sorted(list(map(int, input().split())))
 x = int(input())
-result = 0
-array.sort()
-start, end = 0, len(array)-1
-
-while start < end:
-    curr_sum = array[start] + array[end]
-
-    if curr_sum == x:
-        result += 1
-        start += 1
-        end -= 1
-    elif curr_sum < x:
-        start += 1
-    else:
-        end -= 1
-
-print(result)
+ans = 0
+s, e = 0, n-1
+while s<e:
+    if ls[s] + ls[e] == x:
+        ans += 1
+        s += 1
+        e -= 1
+    elif ls[s] + ls[e] > x:
+        e -= 1
+    elif ls[s] + ls[e] < x:
+        s += 1
+print(ans)
