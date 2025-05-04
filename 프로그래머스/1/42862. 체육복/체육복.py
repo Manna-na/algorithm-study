c7e2f5f30920c3c2b8ba0508fceab1821315b289
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2025-05-05 00:10:26 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,12 +1,20 @@
 def solution(n, lost, reserve):
-    reserve_r = [r for r in reserve if r not in lost]
-    lost_r = [l for l in lost if l not in reserve]
-    reserve_r.sort()
-    lost_r.sort()
-    for r in reserve_r:
-        if r-1 in lost_r:
-            lost_r.remove(r-1)
-        elif r+1 in lost_r:
-            lost_r.remove(r+1)
-        
-    return n - len(lost_r)
+    list = [0]*(n+2)
+    for i in reserve:
+        if i not in lost:
+            list[i] += 1
+    new_lost = []
+    for i in lost:
+        if i not in reserve:
+            new_lost.append(i)
+    answer = n   
+    new_lost.sort()
+    for l in new_lost:
+        print(l)
+        if list[l-1] == 1:
+            list[l-1] -= 1
+        elif list[l+1] == 1:
+            list[l+1] -= 1
+        elif list[l-1] == list[l+1] == 0:
+            answer -= 1
+    return answer
