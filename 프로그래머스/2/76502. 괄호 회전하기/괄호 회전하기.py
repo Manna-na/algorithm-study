eaa9da930e9520a2e2668a621722c3a4875e1390
Hanna Kim <123603435+Manna-na@users.noreply.github.com>
2025-05-04 17:13:03 +0900

    [level 2] Title: 괄호 회전하기, Time: 198.89 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,29 +1,31 @@
-    
+# 1. 괄호 문자열을 회전시키기
+# 2. 괄호 문자열 올바른지 확인
+# 3. 1, 2단계 반복
+# 0: 0-6
+# 1: 1-5 5-6 0-1
 def solution(s):
     answer = 0
-    arr = list(s)
     for i in range(len(s)):
+        str = s[i:]+s[0:i]
         stack = []
-        arr = s[i:len(s)]+s[0:i]
-        for j in arr:
-            if j == "]":
+        for j in range(len(str)):
+            if str[j] in "[({":
+                stack.append(str[j])
+            elif str[j] == "]":
                 if stack and stack[-1] == "[":
                     stack.pop()
                 else:
-                    stack.append(j)
-            elif j == "}":
-                if stack and stack[-1] == "{":
-                    stack.pop()
-                else:
-                    stack.append(j)
-            elif j == ")":
+                    stack.append(str[j])    
+            elif str[j] == ")":
                 if stack and stack[-1] == "(":
                     stack.pop()
                 else:
-                    stack.append(j)
-            else:
-                stack.append(j)
+                    stack.append(str[j])
+            elif str[j] == "}":
+                if stack and stack[-1] == "{":
+                    stack.pop()
+                else:
+                    stack.append(str[j])
         if len(stack) == 0:
-            answer += 1
-            
+            answer += 1               
     return answer
